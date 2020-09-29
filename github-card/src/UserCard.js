bf03ce76b203ea5a7a7bd8d2f75debcf55ce9ccb
Fatima Rizvi <fatimarzv23@gmail.com>
2020-09-29 17:29:20 -0500

    All user data shows up and is displayed, now working on follower's data

---
+++ github-card/src/UserCard.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import './App.css';
+import FollowerCards from './FollowerCards'
+
+//api link https://api.github.com/users/fatima-rizvi
+
+class UserCard extends React.Component {
+  state = {
+    name: '',
+    picture: '',
+    username: 'fatima-rizvi',
+    profile: '',
+    followers: null,
+    followersLink: '',
+    //followersArray: [],
+    following: null,
+    followingLink: ''
+  };
+
+  componentDidMount() {
+    this.fetchUser(this.state.username)
+    // this.fetchFollowersArray(this.state.followersLink)
+    // console.log("Followers url: ",this.state.followersLink);
+  }
+
+//   handleUsernameChange = (e) => {
+//     this.setState({
+//       username: e.target.value
+//     });
+//   };
+
+//   handleSearch = (e) => {
+//     e.preventDefault();
+//     this.fetchUser(this.state.username);
+//   };
+
+  fetchUser = (username) => {
+    fetch(`https://api.github.com/users/${username}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log("Json results: ", data)
+        console.log("Hopefully username: ", data.login)
+        console.log("All followers: ", data.followers_url)
+        this.setState({
+            name: data.name,
+            picture: data.avatar_url,
+            username: data.login,
+            profile: data.html_url,
+            followers: data.followers,
+            followersLink: data.followers_url,
+            following: data.following,
+            followingLink: data.following_url
+        });
+      })
+      .catch((err) => console.log("error: ", err));
+  };
+
+//   fetchFollowersArray = (followersLink) => {
+//     fetch(`${followersLink}`)
+//       .then((res) => res.json())
+//       .then((data) => {
+//         this.setState({
+//             followersArray: data
+//         })
+//         console.log("Json results follow: ", data)
+//       })
+//       .catch((err) => console.log("error: ", err));
+//   };
+
+  render() {
+    return (
+      <div className = 'card'>
+        <img src = {this.state.picture} key = {this.state.picture} alt = 'profile pic' />
+        <div className = 'card-info'>
+            <h3 className = 'username'>Name: {this.state.name}</h3>
+            <p>Username: {this.state.username}</p>
+            <p>Profile: 
+                <a href = {this.state.profile}>{this.state.profile}</a>
+            </p>
+            <p>Followers: {this.state.followers}</p>
+            <p>Following: {this.state.following}</p>
+        </div>
+      </div>
+    )
+  }
+
+};
+
+export default UserCard;
